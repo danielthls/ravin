b3dd337af024fa4e8f458d0b4136d677e09a29e8
FURB\dtlsilva <daniel_thls@yahoo.com.br>
2022-11-18 17:52:59 -0300

    Aula 16-11-2022

--- codigo/database/UdmRavin.pas
+++ codigo/database/UdmRavin.pas
@@ -8,7 +8,7 @@ uses
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
   FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
   FireDAC.Comp.UI, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
-  FireDAC.DApt, FireDAC.Comp.DataSet;
+  FireDAC.DApt, FireDAC.Comp.DataSet, Vcl.Dialogs;
 
 type
   TdmRavin = class(TDataModule)
@@ -16,8 +16,12 @@ type
     drvBancoDeDados: TFDPhysMySQLDriverLink;
     wtcBancoDeDados: TFDGUIxWaitCursor;
     procedure DataModuleCreate(Sender: TObject);
+    procedure cnxBancoDeDadosBeforeConnect(Sender: TObject);
+    procedure cnxBancoDeDadosAfterConnect(Sender: TObject);
   private
     { Private declarations }
+    procedure CriarTabelas();
+    procedure InserirDados();
   public
     { Public declarations }
   end;
@@ -27,13 +31,106 @@ var
 
 implementation
 
-{%CLASSGROUP 'Vcl.Controls.TControl'}
+uses
+  UResourceUtils;
 
+{%CLASSGROUP 'Vcl.Controls.TControl'}
 {$R *.dfm}
+
+procedure TdmRavin.cnxBancoDeDadosAfterConnect(Sender: TObject);
+var
+  LCriarBaseDados: Boolean;
+begin
+  LCriarBaseDados := not FileExists('C:\ProgramData\MySQL\' +
+                                    'MySQL Server 8.0\Data\ravin\pessoa.ibd');
+  if LCriarBaseDados then
+    begin
+      CriarTabelas;
+      InserirDados;
+    end;
+end;
+
+procedure TdmRavin.cnxBancoDeDadosBeforeConnect(Sender: TObject);
+var
+  LCriarBaseDados: Boolean;
+begin
+  LCriarBaseDados := not FileExists('C:\ProgramData\MySQL\' +
+                                    'MySQL Server 8.0\Data\ravin\pessoa.ibd');
+  with cnxBancoDeDados do
+  begin
+    Params.Values['Server'] := 'localhost';
+    Params.Values['User_Name'] := 'root';
+    Params.Values['Password'] := 'root';
+    Params.Values['DriverId'] := 'MySQL';
+    Params.Values['Port'] := '3306';
+
+    if not LCriarBaseDados then
+      begin
+        Params.Values['Database'] := 'ravin';
+      end;
+  end;
+
+end;
+
+procedure TdmRavin.CriarTabelas;
+//var
+  //LSqlArquivoScripts: TStringList;
+  //LCaminhoArquivo: String;
+  //COMPONENTE CRIADO EM TELA
+  //LQuery: TFDQuery;
+begin
+  {*  COMO NÃO DEVE SER FEITO
+  LSqlArquivoScripts := TStringList.Create;
+
+  //LCaminhoArquivo := 'C:\Users\dtlsilva\Documents\ravin\database\createTable.sql';
+  //LSqlArquivoScripts.LoadFromFile(LCaminhoArquivo);
+  //cnxBancoDeDados.ExecSQL(LSqlArquivoScripts.Text);
+  //FreeAndNil(LSqlArquivoScripts);
+
+
+  //COMPONENTE CRIADO EM TELA
+  //LQuery := TFDQuery.Create(Self);
+  //LQuery.Connection := cnxBancoDeDados;
+  //LQuery.SQL.Text := 'SELECT * FROM PESSOAS';*}
+  try
+    cnxBancoDeDados.ExecSQL(TResourceUtils.carregarArquivoResource('createTable.sql','ravin teste'));
+  except
+   on E: Exception do
+    ShowMessage(E.Message);
+
+  end;
+end;
 
 procedure TdmRavin.DataModuleCreate(Sender: TObject);
 begin
-  cnxBancoDeDados.Connected := true;
+  if not cnxBancoDeDados.Connected then
+    begin
+      cnxBancoDeDados.Connected := true;
+    end;
+end;
+
+procedure TdmRavin.InserirDados;
+var
+  LSqlArquivoScripts: TStringList;
+  LCaminhoArquivo: String;
+begin
+  LSqlArquivoScripts := TStringList.Create;
+  LCaminhoArquivo := 'C:\Users\dtlsilva\Documents\ravin\database\inserts.sql';
+  LSqlArquivoScripts.LoadFromFile(LCaminhoArquivo);
+
+  //Trye e pressionar TAB cria automaticamente
+  try
+    cnxBancoDeDados.StartTransaction;
+    cnxBancoDeDados.ExecSQL(TResourceUtils.carregarArquivoResource('createTable.sql','ravin teste'));
+    cnxBancoDeDados.Commit;
+  except
+    on E: Exception do Begin
+      cnxBancoDeDados.Rollback;
+      ShowMessage('Não foi possível concluir o processo!');
+    End;
+  end;
+
+  FreeAndNil(LSqlArquivoScripts);
 end;
 
 end.
---
+++ codigo/utils/UResourceUtils.pas
@@ -0,0 +1,50 @@
+unit UResourceUtils;
+
+interface
+
+USES
+  System.SysUtils, System.IOUtils, System.Classes;
+
+type TResourceUtils = class(TObject)
+  private
+
+  protected
+
+  public
+  class function carregarArquivoResource(PNomeArquivo: String; PNomeAplicacao: String): String;
+end;
+
+implementation
+
+{ TResourceUtils }
+
+class function TResourceUtils.carregarArquivoResource(PNomeArquivo,
+  PNomeAplicacao: String): String;
+var
+  LConteudoArquivo: TStringList;
+  LCaminhoArquivo: String;
+  LCaminhoPastaAplicacao: String;
+  LConteudo: String;
+begin
+  LConteudoArquivo := TStringList.Create();
+  try
+    try
+      LCaminhoPastaAplicacao := TPath.Combine(TPath.GetDocumentsPath, PNomeAplicacao);
+      LCaminhoArquivo := TPath.Combine(LCaminhoPastaAplicacao, PNomeAplicacao);
+
+      LConteudoArquivo.LoadFromFile(LCaminhoArquivo);
+
+      LConteudo := LConteudoArquivo.Text;
+    except
+      on E: Exception do
+        raise Exception.Create('Erro ao carregar os arquivos de resource.' +
+        'Arquivo: ' + PNomeArquivo);
+    end
+  finally
+    LConteudoArquivo.Free;
+  end;
+
+  Result := LConteudo;
+end;
+
+end.
